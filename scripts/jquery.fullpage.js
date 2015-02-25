a76f49f2a256cd19ec008e340767b63c296266c1
laura-naccarato <laura.naccarato@gmail.com>
2015-02-25 16:26:43 -0500

    arrow keys change based on page location. Up arrow displayed when on the last section

--- scripts/jquery.fullpage.js
+++ scripts/jquery.fullpage.js
@@ -6,43 +6,44 @@ fullPage.pagePosition = 0;
 		width: "100vw",
 		height: "100vh"
 	});
-
+	$('.nextPage').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 1000);
+	    return false;
+	});
+	//This hides the scrollbar. Feel free to comment out if you would like the scrollbar to be present on your site
 	document.body.style.overflow = 'hidden';
+
+	//this function goes through all sections and adds the section names to an array.
 	fullPage.getSectionNames = function(){
 		var section = document.getElementsByTagName("section");
 		for (var i = 0; i < section.length ; i++){
 		fullPage.sectionNames.push(section[i].getAttribute("id"));
 		fullPage
 		}
-		// fullPage.getPagePosition();
-		console.log(fullPage.sectionNames);
 	}
-// 	fullPage.getPagePosition = function(){
-// 	for (var i = 0; i < fullPage.sectionNames.length ; i++){
-// 		fullPage.pagePosition[i] = i;
 
-// 	}
-// 	console.log(fullPage.pagePosition);
-// }
-	// $(document).keydown(function(e) {
- //    if(e.which == 40) {
-
-	
-	// }
-	// });
+	//Any links on the page that have a class name of "next page" will be picked up. This function dynamically changes the HREF to link to the next anchor with the same class name.
 	fullPage.buttonScroll = function(){
 	var links = document.getElementsByClassName("nextPage");
+	var up_arrow = $("<img>").attr({src: "images/up_arrow.svg", class: "arrow"});
+	var down_arrow = $("<img>").attr({src: "images/down_arrow.svg", class: "arrow"});
 		for (var i = 0 ; i < links.length ; i++){
 			if ((i + 1) < links.length){
-				$(links[i]).attr("href", "#" + fullPage.sectionNames[i + 1]);
-			} else {
-				// console.log("ah");
-				$(links[i]).attr("href", "#" + fullPage.sectionNames[0]);
-			}
+				$(links[i]).attr("href", "#" + fullPage.sectionNames[i + 1]).addClass("down_arrow");
+				$(".down_arrow").append(down_arrow);
+				
 
+			} else {
+				$(links[i]).attr("href", "#" + fullPage.sectionNames[0]).addClass("up_arrow");
+				$(".up_arrow").append(up_arrow);
+				
+			}
+				console.log(links[i]);
 		}
-			console.log(links);
 	}
+	fullPage.buttonsScroll = function(){}
 	fullPage.getSectionNames();
 	fullPage.buttonScroll();
 }
