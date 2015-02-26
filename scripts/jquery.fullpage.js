48b5dbe74f4baca5a85bdc555b7b9564f53003a9
laura-naccarato <laura.naccarato@gmail.com>
2015-02-26 14:57:55 -0500

    functionality to navigate with up and down arrows has been added.

--- scripts/jquery.fullpage.js
+++ scripts/jquery.fullpage.js
@@ -1,12 +1,25 @@
 $.fn.fullPage = function() {
 var fullPage = {};
 fullPage.sectionNames = [];
-fullPage.pagePosition = 0;
+fullPage.userPosition = 0;
+fullPage.maxPosition;
+
 	$(this).css({
 		width: "100vw",
 		height: "100vh"
 	});
+	// console.log(fullPage.sectionNames);
+	//functionality that allows the user to navigate the page via keypress
+
+
 	$('.nextPage').click(function(){
+
+		fullPage.userPosition++
+		console.log("next page position: " + fullPage.userPosition);
+		// console.log(fullPage.UserPosition);
+		if (fullPage.userPosition > fullPage.maxPosition){
+			fullPage.userPosition = 0;
+		}
 	    $('html, body').animate({
 	        scrollTop: $( $.attr(this, 'href') ).offset().top
 	    }, 1000);
@@ -20,7 +33,8 @@ fullPage.pagePosition = 0;
 		var section = document.getElementsByTagName("section");
 		for (var i = 0; i < section.length ; i++){
 		fullPage.sectionNames.push(section[i].getAttribute("id"));
-		fullPage
+		fullPage.maxPosition = (fullPage.sectionNames.length -1);
+
 		}
 	}
 
@@ -33,17 +47,55 @@ fullPage.pagePosition = 0;
 			if ((i + 1) < links.length){
 				$(links[i]).attr("href", "#" + fullPage.sectionNames[i + 1]).addClass("down_arrow");
 				$(".down_arrow").append(down_arrow);
+ 				// fullPage.userPosition++
 				
 
 			} else {
+				// console.log(fullPage.UserPosition)
 				$(links[i]).attr("href", "#" + fullPage.sectionNames[0]).addClass("up_arrow");
 				$(".up_arrow").append(up_arrow);
 				
 			}
-				console.log(links[i]);
+				// console.log(links[i]);
 		}
 	}
-	fullPage.buttonsScroll = function(){}
+	fullPage.keyScroll = function(){
+			$(document).on("keydown", function(e) {
+			animate = function(){	
+					$('html, body').animate({scrollTop:$("#" + fullPage.sectionNames[fullPage.userPosition]).position().top}, 'slow');
+					}
+			 	if (e.keyCode === 40) {
+			 		console.log("userposition: " + fullPage.userPosition);
+			 			if ((fullPage.userPosition) >= fullPage.maxPosition) {
+			 				fullPage.userPosition = 0;
+			 				animate();
+			 				console.log("down arrow user position reached max userpos: set to 0");
+			 			} else {
+			 				console.log("down arrow else");
+			 				fullPage.userPosition++
+			 				animate();
+			 			}
+
+			 	} else if (e.keyCode === 38){
+			 			if (fullPage.UserPosition < 0 ) {
+			 				fullPage.userPosition = 0;
+			 				console.log("down arrow less than 0 usserpos: " + fullPage.userPosition);
+			 				// console.log("userpos: set to 0");
+			 			} else if (fullPage.userPosition > fullPage.maxPosition){
+			 				console.log("up arrow position greater")
+			 				fullPage.userPosition = 0;
+			 				animate();
+
+			 			} else {
+			 				fullPage.userPosition--;
+			 				animate();
+			 				// console.log("usserpos: " + fullPage.userPosition);
+			 			}
+
+			 	}
+	});
+	}
 	fullPage.getSectionNames();
 	fullPage.buttonScroll();
+	fullPage.keyScroll();
 }
