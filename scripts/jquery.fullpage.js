4f293ae3c943f312f78b2c4a46d88814b604ba57
laura-naccarato <laura.naccarato@gmail.com>
2015-03-01 15:04:50 -0500

    added content to documentation for FullQuery. Also decided to name project fullQuery

--- scripts/jquery.fullpage.js
+++ scripts/jquery.fullpage.js
@@ -1,28 +1,30 @@
 $.fn.fullPage = function(option) {
-var settings = $.extend({
-	animation: false,
-	upArrow: "images/up_arrow.svg",
-	downArrow: "images/up_arrow.svg",
-	animationSpeed: "slow"
-	}, option)
-	console.log(settings);
-var fullPage = {};
-fullPage.animate = function(){	
-	$('html, body').animate({scrollTop:$("#" + fullPage.sectionNames[fullPage.userPosition]).position().top}, settings.animationSpeed);
-}
-fullPage.sectionNames = [];
-fullPage.userPosition = 0;
-fullPage.maxPosition;
-
+	//Settings namespace. Defaults are used if user passes no custom settings
+	var settings = $.extend({
+		animation: false,
+		upArrow: "images/up_arrow.svg",
+		downArrow: "images/up_arrow.svg",
+		animationSpeed: "slow"
+		}, option)
+		console.log(settings);
+	//variable namespace is fullPage
+	var fullPage = {};
+	fullPage.animate = function(){	
+		$('html, body').animate({scrollTop:$("#" + fullPage.sectionNames[fullPage.userPosition]).position().top}, settings.animationSpeed);
+	}
+	fullPage.sectionNames = [];
+	fullPage.userPosition = 0;
+	fullPage.maxPosition;
+	//"this" referring to the section name to be made max width/height
 	$(this).css({
 		width: "100vw",
 		height: "100vh"
 	});
-
+	//the following function is for moving from section to section. userPosition is a global variable that tracks which sectionNames
+	//the user is on, so that you can move through the links, or buttons, and not have any problems with going to the wrong section
 	fullPage.nextPageClick = function(){	
 		$('.nextPage').on("click", function(){
 			fullPage.userPosition++
-
 			if (fullPage.userPosition > fullPage.maxPosition){
 				fullPage.userPosition = 0;
 			}
@@ -32,7 +34,7 @@ fullPage.maxPosition;
 	//This hides the scrollbar. Feel free to comment out if you would like the scrollbar to be present on your site
 	document.body.style.overflow = 'hidden';
 
-	//this function goes through all sections and adds the section names to an array.
+	//this function goes through all sections and adds the section names to an array called fullpage.sectionNames.
 	fullPage.getSectionNames = function(){
 		var section = document.getElementsByTagName("section");
 		for (var i = 0; i < section.length ; i++){
@@ -42,25 +44,28 @@ fullPage.maxPosition;
 		}
 	}
 
-	//Any links on the page that have a class name of "next page" will be picked up. This function dynamically changes the HREF to link to the next anchor with the same class name.
+	// fullPage.scroll = function(){
+	// 	(window).on("scroll", "section", function(){
+	// 		$('html, body').animate({scrollTop:$("#" + fullPage.sectionNames[fullPage.userPosition]).position().top}, settings.animationSpeed);
+	// 	});
+	// }
+	
+
+	//Any links on the page that have a class name of "next page" will be scanned. This function dynamically changes the HREF to link to the next 
+	//anchor with the same class name.
 	fullPage.buttonScroll = function(){
 	var links = document.getElementsByClassName("nextPage");
 	var up_arrow = $("<img>").attr({src: settings.upArrow, class: "arrow"});
 	var down_arrow = $("<img>").attr({src: settings.downArrow, class: "arrow"});
-		
+	//	The following logic is responsible for setting the link anchors. Also, the arrows are assigned based on position. a
 		for (var i = 0 ; i < links.length ; i++){
-
 			if ((i + 1) < links.length){
 				$(links[i]).attr("href", "#" + fullPage.sectionNames[i + 1]).addClass("down_arrow");
 				$(".down_arrow").append(down_arrow);
- 				
-				
-
 			} else {
 				// console.log(fullPage.UserPosition)
 				$(links[i]).attr("href", "#" + fullPage.sectionNames[0]).addClass("up_arrow");
 				$(".up_arrow").append(up_arrow);
-				
 			}
 			if (settings.animation === true){
 				$(".arrow").addClass("animate");
@@ -101,5 +106,6 @@ fullPage.maxPosition;
 	fullPage.getSectionNames();
 	fullPage.buttonScroll();
 	fullPage.nextPageClick();
+	// fullPage.scrollPage(	);
 	fullPage.keyScroll();
 }
